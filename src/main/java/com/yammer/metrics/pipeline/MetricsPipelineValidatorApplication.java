9d5e4c4a8c7e893dfb9f5d9d11449da907a2340f
Chris Shellenbarger <chris.shellenbarger@gmail.com>
2015-11-18 22:11:42 -0800

    Initial version of project as generated by Spring Initializer.

---
+++ src/main/java/com/yammer/metrics/pipeline/MetricsPipelineValidatorApplication.java
@@ -0,0 +1,12 @@
+package com.yammer.metrics.pipeline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MetricsPipelineValidatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MetricsPipelineValidatorApplication.class, args);
+    }
+}
